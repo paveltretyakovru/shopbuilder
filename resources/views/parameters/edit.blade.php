f9cfbb4ac8703def3aab089f8d95c5a0e35e5332
Tretjakov Pavel <tretjakov.pavel@gmail.com>
2015-03-04 22:11:58 +0300

    Начало работы - контроллер внешнего вида товара

--- resources/views/parameters/edit.blade.php
+++ resources/views/parameters/edit.blade.php
@@ -3,7 +3,8 @@
 @section('breadcrumb')
 	<li>{!! link_to('admin' , "Административная панель") !!}</li>
 	<li>{!! link_to('products' , 'Продукция') !!}</li>
-	<li>{!! link_to_route('products.edit' , $product->title , [$product->id]) !!}</li>
+	<li>{!! link_to_route('products.show' , $product->title , [$product->id]) !!}</li>
+	<li>{!! link_to_route('products.edit' , 'Редактор' , [$product->id]) !!}</li>
 	<li class="active">Параметры</li>
 @stop
 
