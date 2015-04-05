5a28cae595e6be12aa8a8324900be6e93c3f35ce
Pavel Tretyakov <tretjakov.pavel@gmail.com>
2015-04-05 05:20:52 +0300

    Создана валидация категории товаров в url

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -4,6 +4,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
+use Validator;
 
 use App\Category;
 use App\Product;
@@ -11,9 +12,31 @@ use App\Product;
 class ProductsController extends Controller {
 
 	public function index($type){
-		if ($type != 'phones') {
+
+		// Обращается ли пользователь к существующей категории товаров
+		if ($this->checkCategory($type)) {
+			return "test check";
+		}else{
+			// Если данной категоии нет, значит возвращаем страницу 404
+			// Остальные маршруты описываются в routes.php
 			return view('errors.404');
 		}
+	}
+
+	/* Проверям идет ли обращение к существующей категории товаров */
+	private function checkCategory($category){
+		
+		$validator = Validator::make(
+			['url' => $category] ,
+			['url' => 'required|exists:categories,url']
+		);
+
+
+		if ($validator->fails()) {
+			return false;
+		}else{
+			return true;
+		}	
 	}
 
 }
